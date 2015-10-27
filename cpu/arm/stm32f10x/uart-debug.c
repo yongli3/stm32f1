4924c415446a8d9bc85699a1d90c4a18372f58a4
??? <xukai@wsn.cn>
2015-10-27 10:46:01 +0800

    ????

---
+++ cpu/arm/stm32f10x/uart-debug.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "stm32f10x.h"
+#include "uart-debug.h"
+
+void debug_init(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+    GPIO_InitTypeDef GPIO_InitStructure;
+    
+    //使能GPIOA时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_USART1, ENABLE);
+    
+    //PA9 TX1 复用推挽输出
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    //PA10 RX1 浮动输入
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;    
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART1, &USART_InitStructure);
+    
+    //使能USART1
+    USART_Cmd(USART1, ENABLE);
+}
+
+int fputc(int ch, FILE * f)
+{
+    USART_SendData(USART1, (uint8_t)ch);
+    while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET ); 
+    return ch;
+}
---
+++ cpu/arm/stm32f10x/uart-debug.h
@@ -0,0 +1,6 @@
+#ifndef __UART_DEBUF_H__
+#define __UART_DEBUF_H__
+
+void debug_init(void);
+
+#endif
