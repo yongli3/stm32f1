e5dcda29c35c5c3f68697f1ed14102797aa321ee
?? <??@localhost>
2015-10-29 16:21:28 +0800

    ????

--- examples/stm32dk/hello-world/hello-world.c
+++ examples/stm32dk/hello-world/hello-world.c
@@ -6,23 +6,23 @@ AUTOSTART_PROCESSES(&hello_world_process);
 
 PROCESS_THREAD(hello_world_process, ev, data)
 {
-	static struct etimer timer;
-	
-	PROCESS_BEGIN();
+    static struct etimer timer;
+    
+    PROCESS_BEGIN();
 
-	printf("Hello, World\r\n");
+    printf("Hello, World\r\n");
 
-	etimer_set(&timer, CLOCK_CONF_SECOND / 2);
+    etimer_set(&timer, CLOCK_CONF_SECOND / 2);
   
-	for (;;) 
+    for (;;) 
     {
-		// 等待定时器事件
-		PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+        // 等待定时器事件
+        PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
         printf("A");
         
-		// 重置定时器
-		etimer_reset(&timer);
-	}
+        // 重置定时器
+        etimer_reset(&timer);
+    }
   
-	PROCESS_END();
+    PROCESS_END();
 }
